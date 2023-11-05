1aacb684cf8242b4a660d498fca70fea90d80689
GiuseppeBuonasera <121633690+GiuseppeBuonasera@users.noreply.github.com>
2023-11-05 23:00:31 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -3,42 +3,135 @@
 //vedere quali libri sono disponibili 
 #include <stdio.h>
 #include <stdlib.h>
+#include "catalogo.h"
 
-void menu(){
+nodocategorie *listacategorie = NULL;
+
+void menu_principale(void){
+    int i;
     system("clear");
     printf("//////////////////////////////////////////////////////////////\n");
     printf("# BENVENUTO NELL' APPLICAZIONE DELLA LIBRERIA 'PIAZZA DUOMO'\n");
     printf("#                COME DESISDERI ACCEDERE:\n");
-    printf(" 1)ADMIN\n 2)UTENTE\n 3)ESCI\n");
+    printf(" 1)UTENTE\n 2)ADMIN\n 3)ESCI\n");
+    scanf("%d", &i);
+    switch (i){
+    case 1:
+        menu_utente();
+        break;
+    
+    case 2:
+        menu_admin();
+        break;
+    default:
+        exit(0);
+
+    }
     
 }    
 
     
-void menu_admin(){
+void menu_admin(void){
+    int i;
     system("clear");
     printf("/////////////////////////////////////////////////////\n");
     printf("#      MENÙ ADMIN DELLA LIBRERIA 'PIAZZA DUOMO'\n");
     printf("#       QUALE OPERAZIONE DESISDERI EFFETTUARE?\n");
-    printf(" 1) AGGIUNGI UN LIBRO\n 2) RIMUOVI UN LIBRO\n 3) MODIFICA LIBRO\n 4) VEDI TUTTI I LIBRI PRESENTI\n 5) GESTISCI LA CASSA\n 6) ESCI\n");        
+    printf(" 1) AGGIUNGI UN LIBRO\n 2) RIMUOVI UN LIBRO\n 3) MODIFICA LIBRO\n 4) VEDI TUTTI I LIBRI PRESENTI\n 5) GESTISCI LA CASSA\n 6) TORNA INDIETRO\n");        
+    scanf("%d", &i);
+    switch (i){
+    case 1:
+        menu_admin_aggiunta_libro();
+        break;
+    case 2:
+        menu_admin_rimozione_libro();
+    case 3:
+        menu_admin_modifica_libro();
+        break;
+    case 4:
+        stampalibri(listacategorie);
+    
+    case 5:
+        menu_cassa();
+    default:
+        menu_principale();
+    }
 }
 
-void menu_admin_aggiunta_libro(){
+
+void menu_admin_aggiunta_libro(void){
+    char categoria;
+    char libro;
+    char autore;
     system("clear");
     printf("/////////////////////////////////////////////////////\n");
     printf("#      MENÙ ADMIN DELLA LIBRERIA 'PIAZZA DUOMO'\n");
-    printf("#       QUALE OPERAZIONE DESISDERI EFFETTUARE?\n");
-    printf("#                        ");
+    printf("#  SELEZIONA LA CATEGORIA ALLA QUALE VUOI AGGIUNGERE IL LIBRO\n");
+    scanf("%c", &categoria);
+    printf("# SELEZIONA IL TITOLO DEL LIBRO");
+    scanf("%c", &libro);
+    printf("# SELEZIONA L'AUTORE DEL LIBRO");
+    scanf("%c", &autore);
+    aggiungi_libro_a_categoria(categoria, libro,  autore);
+    menu_admin();
 }
 
-void menu_utente(){
+
+void menu_admin_rimozione_libro(void){
+    char libro_cercato;
+    system("clear");
+    printf("/////////////////////////////////////////////////////\n");
+    printf("#      MENÙ ADMIN DELLA LIBRERIA 'PIAZZA DUOMO'\n");
+    printf("#           QUALE LIBRO DESIDERI RIMUOVERE?  \n");
+    scanf("%c", &libro_cercato);
+    elimina_libro(listacategorie, libro_cercato);
+    menu_admin();
+}
+
+
+void menu_admin_modifica_libro(void){
+    char libro_cercato;
+    char nuovo_libro;
+    char nuovo_autore;
+    system("clear");
+    printf("/////////////////////////////////////////////////////\n");
+    printf("#      MENÙ ADMIN DELLA LIBRERIA 'PIAZZA DUOMO'\n");
+    printf("#           QUALE LIBRO DESIDERI MODIFICARE?  \n");
+    scanf("%c", &libro_cercato);
+    printf("#           AGGIUNGI IL NUOVO TITOLO  \n");
+    scanf("%c", &nuovo_libro);
+    printf("#           AGGIUNGI IL NUOVO AUTORE");
+    scanf("%c", &nuovo_autore);
+    modifica_libro(listacategorie, libro_cercato, nuovo_libro, nuovo_autore);
+    menu_admin();
+}
+
+
+void menu_utente(void){
+    int i;
     system("clear");
     printf("/////////////////////////////////////////////////////\n");
     printf("#     MENÙ UTENTE  DELLA LIBRERIA 'PIAZZA DUOMO'\n");
     printf("#       QUALE OPERAZIONE DESISDERI EFFETTUARE?\n");
     printf(" 1) VEDI I LIBRI PRESENTI NEL CATALOGO \n 2) COMPRA UN LIBRO\n 3) ESCI\n"); 
+    scanf("%c", &i);
+    switch (i){
+    case 1:
+        stampalibri(listacategorie);
+    case 2: 
+          
+  
+
+
+        break;
+    
+    default:
+        break;
+    }
+
 }
 
-void menu_cassa_admin(){
+void menu_cassa_admin(void){
     system("clear");
     printf("/////////////////////////////////////////////////////\n");
     printf("#      MENÙ  DELLA LIBRERIA 'PIAZZA DUOMO'\n");
@@ -48,7 +141,7 @@ void menu_cassa_admin(){
  
 }
 
-void menu_cassa_utente(){
+void menu_cassa_utente(void){
    system("clear");
    printf("/////////////////////////////////////////////////////\n");
    printf("#      MENÙ  DELLA LIBRERIA 'PIAZZA DUOMO'\n");
@@ -61,39 +154,7 @@ void menu_cassa_utente(){
 }
 
 int main(){
-    int num;
-    menu();
-    scanf("%d",&num);
-    switch(num){
-        
-        case 1:
-        menu_admin();
-        scanf("%d",&num);
-        switch(num){
-            case 4:
-            menu_cassa_admin();
-            break;
-    
-        }
-        break;
-
-    
-        case 2:
-        menu_utente();
-        scanf("%d",&num);
-        switch(num){
-            case 2:
-            menu_cassa_utente();
-            break;
-            }
-        break;
-
-
-        case 3:
-        exit;
-        break;
-    }
-}     
+}    
 
     
     
